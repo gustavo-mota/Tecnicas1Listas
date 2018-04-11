9f1744d8c2c4bf0b9a3527bedbe8cc17e33da046
Gustavo1001 <gustavomotax2@gmail.com>
2018-04-11 13:35:21 -0300

    Update ContaXPTOPlus.java

--- lista_4/ContaXPTOPlus.java
+++ lista_4/ContaXPTOPlus.java
@@ -6,7 +6,7 @@ public class ContaXPTOPlus extends Contas{
 	}
 
 	public String toString(){
-		return "\nTipo: Basic\nSaldo: R" + saldo + "\n Id: "+ id;
+		return "\nTipo: Plus\nSaldo: R" + saldo + "\n Id: "+ id;
 	}
 	@Override
 	public float creditar(float valor){
