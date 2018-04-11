333a41ef8a4c24af987ebec9898616e6f651c2ed
Gustavo1001 <gustavomotax2@gmail.com>
2018-04-11 13:31:01 -0300

    Update Sistema.java

--- lista_4/Sistema.java
+++ lista_4/Sistema.java
@@ -2,7 +2,6 @@ class Sistema{
 	Contas[] contas;
 	public Sistema(){
 		this.contas = new Contas[10];
-		//contas[0] = new ContaXPTOBasic();
 	}
 
 	public void criarContaXPTOBasic(int i){
@@ -17,16 +16,16 @@ class Sistema{
         this.contas[i] = new ContaXPTOExtreme(i); 
     }
 
-	public float consultarSaldo(int i){ //pq public
-		return this.contas[i].consultarSaldo();//esta conta, indice i, ao consultarSaldo que mexe com as contas
+	public float consultarSaldo(int i){
+		return this.contas[i].consultarSaldo();//sobrecarga
 
 	}
 
-	public void creditar(int i, float valor){//Sobrecarga, pois em conta há o método creditar recebendo apenas o valor
+	public void creditar(int i, float valor){//Sobrecarga
         this.contas[i].creditar(valor);
     }
 
-    public void debitar(int i, float valor){//Sobrecarga, pois em conta há o método debitar recebendo apenas o valor
+    public void debitar(int i, float valor){//Sobrecarga
         this.contas[i].debitar(valor);
     }
 }
